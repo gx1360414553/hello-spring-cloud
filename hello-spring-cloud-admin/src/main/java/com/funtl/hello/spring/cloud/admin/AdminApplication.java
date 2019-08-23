0cd94eda2ba36fe8a85ed6ffd5aa08d3b05600ef
gx1360414553 <1360414553@qq.com>
2019-08-23 11:01:38 +0800

    分布式配置中心ok

---
+++ hello-spring-cloud-admin/src/main/java/com/funtl/hello/spring/cloud/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 高雄
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2019年08月23日 09:46:00
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ hello-spring-cloud-zipkin/src/main/java/com/funtl/hello/spring/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @author 高雄
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2019年08月23日 09:02:00
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
